37c24e073f66aff86f003098ebed7f79daf775b0
andthum <56444687+andthum@users.noreply.github.com>
2023-06-21 10:02:12 +0200

    [prepare_sim_dir.sh]: Fix Wrong Labeling of Required/Optional Arguments

--- bash/prepare_sim_dir.sh
+++ bash/prepare_sim_dir.sh
@@ -6,7 +6,8 @@
 
 information() {
     echo "Prepare the simulation directory for each transferred lithium ion by"
-    echo "copying the parameter and topology files to the directory."
+    echo "copying the parameter and topology files to the corresponding"
+    echo "directories."
 }
 
 usage() {
@@ -14,25 +15,28 @@ usage() {
     echo "Usage:"
     echo
     echo "Required arguments:"
-    echo "  -h    Show this help message and exit."
     echo "  -s    The name of the system to simulate, e.g."
     echo "        lintf2_g1_20-1_gra_q1_sc80."
+    echo
+    echo "Optional arguments:"
+    echo "  -h    Show this help message and exit."
 }
 
 ########################################################################
 # Argument Parsing                                                     #
 ########################################################################
 
-while getopts hs: option; do
+while getopts s:h option; do
     case ${option} in
+        # Required arguments.
+        s)
+            system=${OPTARG}
+            ;;
         # Optional arguments.
         h)
             information
             usage
             exit 0
-            ;;
-        s)
-            system=${OPTARG}
             ;;
         # Handling of invalid options or missing arguments.
         *)
